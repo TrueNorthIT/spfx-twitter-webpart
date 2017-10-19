c067ea9e6b0807ccf88f68583bc2e30894faf1ff
Steve Drake <sdrake@trueNorthIT.co.uk>
2017-10-19 14:09:37 +0100

    moved twitter load code to render.

--- src/webparts/twitter/TwitterWebPart.ts
+++ src/webparts/twitter/TwitterWebPart.ts
@@ -2,40 +2,48 @@ import { Version } from '@microsoft/sp-core-library';
 import {
   BaseClientSideWebPart,
   IPropertyPaneConfiguration,
-  PropertyPaneTextField
+  PropertyPaneTextField,
+  PropertyPaneLabel
 } from '@microsoft/sp-webpart-base';
 import { escape } from '@microsoft/sp-lodash-subset';
 
 import styles from './TwitterWebPart.module.scss';
 import * as strings from 'TwitterWebPartStrings';
 import { SPComponentLoader } from '@microsoft/sp-loader';
+import "widgets";
 
 export interface ITwitterWebPartProps {
   screenname: string;
+  tweetlimit:number;
+  height:number;
 }
 
 export default class TwitterWebPartWebPart extends BaseClientSideWebPart<ITwitterWebPartProps> {
+  widget:any;
   public constructor() {
     super();
-    let widget = require("widgets");
-    setTimeout( () => widget.widgets.load(), 0);
+    this.widget = require("widgets");
   }
-
   public render(): void {
     this.domElement.innerHTML = `
       <div class="${styles.twitter}">
         <div class="${styles.container}">
-        hello
             <a class="twitter-timeline"
+            data-tweet-limit="${this.properties.tweetlimit}"
+            data-height="${this.properties.height}"
             href="https://twitter.com/${escape(this.properties.screenname)}">
             Tweets by @${escape(this.properties.screenname)}
             </a>
         </div>
       </div>`;
+      setTimeout( () => this.widget.widgets.load(), 0);
   }
 
   protected get dataVersion(): Version {
     return Version.parse('1.0');
+  }
+  protected get disableReactivePropertyChanges(): boolean {
+    return true;
   }
 
   protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
@@ -51,7 +59,14 @@ export default class TwitterWebPartWebPart extends BaseClientSideWebPart<ITwitte
               groupFields: [
                 PropertyPaneTextField('screenname', {
                   label: 'Screenname'
-                })
+                }),
+                PropertyPaneTextField('tweetlimit', {
+                  label: 'Tweet limit'
+                }),
+                PropertyPaneTextField('height', {
+                  label: 'Height'
+                }),
+                PropertyPaneLabel("height", { text: "Height is ignored if tweet limit is set"} ),
               ]
             }
           ]
